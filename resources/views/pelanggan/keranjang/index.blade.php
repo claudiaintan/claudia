10716c4a17a90d4333674a2774615710ade6223c
YahyaKhaliman <yahyaindrayana35@gmail.com>
2024-09-14 15:20:20 +0700

    pemberitahuan

--- resources/views/pelanggan/keranjang/index.blade.php
+++ resources/views/pelanggan/keranjang/index.blade.php
@@ -244,7 +244,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="modalLabel">Bukti Pembayaran</h5>
+                    <h5 class="modal-title" id="modalLabel">Konfirmasi Pesanan</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
